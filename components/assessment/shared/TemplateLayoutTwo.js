cef7fcc0931ccecc0b90c1b438eb20e5a9a75a26
Vaibhav <vaibhavgstpl@gmail.com>
2021-01-14 20:47:34 +0530

    feat: Add select homepage layout step

---
+++ components/assessment/shared/TemplateLayoutTwo.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import PropTypes from 'prop-types';
+import './style.sass'
+
+const TemplateLayoutTwo = (props) => {
+    return(
+        <div className="template2">
+            <div className='t-header'></div>
+            <div className='t-content'></div>
+        </div>
+        
+    )
+}
+TemplateLayoutTwo.propTypes = {
+    setOpen: PropTypes.func
+};
+export default TemplateLayoutTwo;
