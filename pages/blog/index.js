f44b50f71bf9c55e25f2130be24576bb125172a0
palak <pgupta@isystango.com>
2021-01-20 17:10:56 +0530

    Working on create blog screen.

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,5 +1,24 @@
 import React from 'react';
 import RichTextEditor from './rte';
+import  Link from 'next/link'
+import { 
+  DashboardMenuIcon,
+  EditSiteMenuIcon,
+  BlogMenuIcon,
+  MarketingMenuIcon,
+  Facebook,
+  LinkedIn,
+  Twitter,
+  YouTube,
+  Instagram
+} from '../../utils/svg'
+
+import { 
+  Form, 
+  Row,
+  ProgressBar, 
+} from 'react-bootstrap';
+import profilePic from '../../public/images/media/media-1.jpg';
 
 const BlogPage =(props) => {
 
@@ -41,9 +60,114 @@ const BlogPage =(props) => {
       ]
 
     return(
-        <section className="main-section">
-            <section className="page-section"><RichTextEditor readOnly={false} initialValue={initialValue} /></section>
-        </section>
+      <section className="dashboard-wrapper">
+        <aside className="dashboard-menu">
+          <ul>
+            <li>
+              <a href="/">
+              Dashboard
+                <DashboardMenuIcon />
+              </a>
+            </li>
+            <li>
+              <a href="/">
+              Edit Site
+                <EditSiteMenuIcon />
+              </a>
+            </li>
+            <li>
+              <a href="/">
+              Blog
+                <BlogMenuIcon />
+              </a>
+            </li>
+            <li>
+              <a href="/">
+              Marketing
+                <MarketingMenuIcon />
+              </a>
+            </li>
+          </ul>
+        </aside>
+        <main className="dashboard-data">
+          <section className="dashboard-body">
+            <div className="blog-creation">
+              <Form>  
+                <div className="blog-creation-head">
+                  <div className="blog-creation-head-left">
+                  <Form.Group className="blog-title-group">
+                        <Form.Control  placeholder='Blog post title'/>
+                        <Form.Text>
+                        March 25, 2020 / 4 min read / Author Name
+                        </Form.Text>
+                    </Form.Group>
+                    <div className="upload-feature-img-wrap">
+                    <div className="upload-feature-img">
+                      Click here to edit feature image
+                      </div>
+                    </div>
+                  </div>
+                  <div className="blog-creation-author-box">
+                    <h5>Author Box</h5>
+                    <div className="author-info">
+                      <div className="author-img">
+                        <img src={profilePic} alt="Jason Miller" />
+                      </div>
+                      <div className="author-name">
+                          <span>Jason Miller</span>
+                          <a>+ Add Jason</a>
+                      </div>
+                    </div>
+                    <div className="author-bio">
+                      <h6>About</h6>
+                      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                    </div>
+                    <ul className="author-social-links">
+                      <li className="facebook">
+                        <a>
+                          <Facebook />
+                        </a>
+                      </li>
+                      <li className="linkedin">
+                        <a>
+                          <LinkedIn />
+                        </a>
+                      </li>
+                      <li className="twitter">
+                        <a>
+                          <Twitter />
+                        </a>
+                      </li>
+                      <li className="youtube">
+                        <a>
+                          <YouTube />
+                        </a>
+                      </li>
+                      <li className="instagram">
+                        <a>
+                          <Instagram />
+                        </a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+                <div className="blog-creation-content">
+                  <div className="word-count">
+                    <div className="word-count-progressbar">
+                      <label>Word Count: </label>
+                    <ProgressBar now="72" label="357/500" />
+                    </div>
+                  
+                  </div>
+                  <div className="blog-editor">
+                  <RichTextEditor readOnly={false} initialValue={initialValue} />
+                  </div>
+                </div>
+              </Form>
+            </div>
+          </section>
+        </main>
+      </section>
     )
 }
 
