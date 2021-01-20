33da96e3131749e57ebe8c3c7c7b7f03c3c5c9a0
Vaibhav <vaibhavgstpl@gmail.com>
2021-01-20 11:48:58 +0530

    Worked on basic slate integration.

---
+++ pages/blog/index.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import RichTextEditor from './rte';
+
+const BlogPage =(props) => {
+
+    const initialValue = [
+        {
+          type: 'paragraph',
+          children: [
+            { text: 'This is editable ' },
+            { text: 'rich', bold: true },
+            { text: ' text, ' },
+            { text: 'much', italic: true },
+            { text: ' better than a ' },
+            { text: '<textarea>', code: true },
+            { text: '!' },
+          ],
+        },
+        {
+          type: 'paragraph',
+          children: [
+            {
+              text:
+                "Since it's rich text, you can do things like turn a selection of text ",
+            },
+            { text: 'bold', bold: true },
+            {
+              text:
+                ', or add a semantically rendered block quote in the middle of the page, like this:',
+            },
+          ],
+        },
+        {
+          type: 'block-quote',
+          children: [{ text: 'A wise quote.' }],
+        },
+        {
+          type: 'paragraph',
+          children: [{ text: 'Try it out for yourself!' }],
+        },
+      ]
+
+    return(
+        <section className="main-section">
+            <section className="page-section"><RichTextEditor readOnly={false} initialValue={initialValue} /></section>
+        </section>
+    )
+}
+
+export default BlogPage;
