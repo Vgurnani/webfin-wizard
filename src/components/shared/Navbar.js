ae0e8dccbc616ba86e31f96fcdb06a216295517c
palak <pgupta@isystango.com>
2021-01-25 19:24:17 +0530

    Worked on menu responsive design.

--- src/components/shared/Navbar.js
+++ src/components/shared/Navbar.js
@@ -1,12 +1,17 @@
 import React, { useState } from 'react'
-import { Link } from "react-router-dom";
+//import Link  from 'next/link'
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import webFinLogo from '../../public/images/header/webFin-logo.svg';
+import webFinLogo from '../../images/header/webFin-logo.svg';
+import headerProfilePic from '../../images/media/media-1.jpg';
 import { isLoggedIn } from '../../utils/helpers'
 import { logoutUser } from '../../middleware/auth';
 import { useDispatch } from 'react-redux'
+import {
+  NotificationIcon,
+  ChevronRight,
+} from '../../utils/svg';
 
 const Navbar = (props) => {
   const dispatch  = useDispatch();
@@ -22,37 +27,81 @@ const Navbar = (props) => {
     } else {
       return '';
     }
-
+    
   }
     const logout = () => {
-      dispatch(logoutUser())
+      dispatch(logoutUser()) 
     }
-    const pathname = window.location.pathname
+    const { pathname } = props
     const LinkView = () => {
       if(isLoggedIn()){
-        return<button className="btn btn-secondary" onClick={logout} >logout</button>
+        return(
+          <ul className="main-navigation">
+            <li>
+              <a  href="">All Sites</a>
+            </li>
+            <li>
+              <a  href="">Domains</a>
+            </li>
+            <li>
+              <a  href="">Support</a>
+            </li>
+            <li>
+              <a  href=""><NotificationIcon /></a>
+            </li>
+            <li className="header-profile-img">
+              <a href="">
+                <span className="nav-profile-pic">
+                  <img src={headerProfilePic} alt="John" />
+                </span>
+                <ChevronRight />
+              </a>
+              <ul className="header-submenu">
+                <li>
+                <a onClick={logout} >logout</a>
+                </li>
+              </ul>
+            </li>
+          </ul>
+        )
+      } else{
+        return(
+          <ul className="main-navigation">
+            <li>
+              <a href="/login" className="btn btn-secondary">
+                login
+              </a>
+            </li>
+          </ul>
+        )
       }
-      else if(pathname === '/'){
-        return(<Link to="/login">
-          <a className="btn btn-secondary">login</a>
-        </Link>)
-      }else{
-        return null
-      }
-
+      // else if(pathname === '/'){
+      //   return(
+      //   <Link  href="/login">
+      //     <a className="btn btn-secondary">login</a>
+      //   </Link>
+      //   )
+      // }
+      
+       
     }
     return (
       <header className={`main-header ${btAction(pathname)}`}>
-        <Container>
+        <Container>     
           <Row className="header-top">
             <Col className="header-logo">
               <div className="navbar-brand">
-                <Link to="/" className="navbar-item" title="Logo">
-                  <img src={webFinLogo} alt="WebFin" />
-                </Link>
-              </div>
+                <a href="/" className="navbar-item" title="Logo">
+                  <img src={webFinLogo} alt="WebFin" />                               
+                </a>           
+              </div>                                     
             </Col>
             <Col className="header-menu">
+              <div class="mobile-humberger">
+                <div class="bar1"></div>
+                <div class="bar2"></div>
+                <div class="bar3"></div>
+              </div>
               <nav
                 className="navbar is-transparent"
                 role="navigation"
