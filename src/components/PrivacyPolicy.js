71549bfc1bd5291e841cde19cb2c9f7ebddcca86
palak <pgupta@isystango.com>
2021-02-02 15:12:25 +0530

    Worked on privacy policy page.

--- src/components/PrivacyPolicy.js
+++ src/components/PrivacyPolicy.js
@@ -57,7 +57,7 @@ const PrivacyPolicyPage = (props) => {
                    <a href="#table-contents-15">HOW CAN YOU REVIEW, UPDATE OR DELETE THE DATA WE COLLECT FROM YOU?</a>
                </li>
            </ol>
-           <div id="table-contents-1">
+           <div class="privacy-table-contents" id="table-contents-1">
                <h3>1. WHAT INFORMATION DO WE COLLECT?</h3>
                <h4>Personal information you disclose to us</h4>
                <p><i><b>In Short:  </b>We collect personal information that you provide to us.</i></p>
@@ -80,7 +80,7 @@ const PrivacyPolicyPage = (props) => {
                 </ul>
 
            </div>
-           <div id="table-contents-2">
+           <div class="privacy-table-contents" id="table-contents-2">
                <h3>2. HOW DO WE USE YOUR INFORMATION?</h3>
                <p><i><b>In Short:</b> We process your information for purposes based on legitimate business interests, the fulfillment of our contract with you, compliance with our legal obligations, and/or your consent.</i></p>
                <p>We use personal information collected via our Services for a variety of business purposes described below. We process your personal information for these purposes in reliance on our legitimate business interests, in order to enter into or perform a contract with you, with your consent, and/or for compliance with our legal obligations. We indicate the specific processing grounds we rely on next to each purpose listed below.</p>
@@ -108,7 +108,7 @@ const PrivacyPolicyPage = (props) => {
 
 
            </div>
-           <div id="table-contents-3">
+           <div class="privacy-table-contents" id="table-contents-3">
                <h3>3. WILL YOUR INFORMATION BE SHARED WITH ANYONE?</h3>
                <p><i><b>In Short:</b>   We only share information with your consent, to comply with laws, to provide you with services, to protect your rights, or to fulfill business obligations.</i></p>
                <p>We may process or share your data that we hold based on the following legal basis:</p>
@@ -129,7 +129,7 @@ const PrivacyPolicyPage = (props) => {
                     <li><b>Offer Wall.</b> Our App may display a third-party hosted "offer wall." Such an offer wall allows third-party advertisers to offer virtual currency, gifts, or other items to users in return for the acceptance and completion of an advertisement offer. Such an offer wall may appear in our App and be displayed to you based on certain data, such as your geographic area or demographic information. When you click on an offer wall, you will be brought to an external website belonging to other persons and will leave our App. A unique identifier, such as your user ID, will be shared with the offer wall provider in order to prevent fraud and properly credit your account with the relevant reward. Please note that we do not control third-party websites and have no responsibility in relation to the content of such websites. The inclusion of a link towards a third-party website does not imply an endorsement by us of such website. Accordingly, we do not make any warranty regarding such third-party websites and we will not be liable for any loss or damage caused by the use of such websites. In addition, when you access any third-party website, please understand that your rights while accessing and using those websites will be governed by the privacy notice and terms of service relating to the use of those websites.</li>
                </ul>
            </div>
-           <div id="table-contents-4">
+           <div class="privacy-table-contents" id="table-contents-4">
                <h3>4. WHO WILL YOUR INFORMATION BE SHARED WITH?</h3>
                <p><i><b>In Short:</b> We only share information with the following third parties.</i></p>
                <p>We only share and disclose your information with the following third parties. We have categorized each party so that you may be easily understand the purpose of our data collection and processing practices. If we have processed your data based on your consent and you wish to revoke your consent, please contact us using the contact details provided in the section below titled <a href="#table-contents-14">"HOW CAN YOU CONTACT US ABOUT THIS NOTICE?"</a>.</p>
@@ -192,34 +192,34 @@ const PrivacyPolicyPage = (props) => {
                    </li>
                </ul>
            </div>
-           <div id="table-contents-5">
+           <div class="privacy-table-contents" id="table-contents-5">
                <h3>5. DO WE USE COOKIES AND OTHER TRACKING TECHNOLOGIES?</h3>
                <p><i><b>In Short:</b>   We may use cookies and other tracking technologies to collect and store your information.</i></p>
                <p>We may use cookies and similar tracking technologies (like web beacons and pixels) to access or store information. Specific information about how we use such technologies and how you can refuse certain cookies is set out in our Cookie Notice.</p>
            </div>
-           <div id="table-contents-6">
+           <div class="privacy-table-contents" id="table-contents-6">
                <h3>6. HOW DO WE HANDLE YOUR SOCIAL LOGINS?</h3>
                <p><i><b>In Short:</b>   If you choose to register or log in to our services using a social media account, we may have access to certain information about you.</i></p>
                <p>Our Services offers you the ability to register and login using your third-party social media account details (like your Facebook or Twitter logins). Where you choose to do this, we will receive certain profile information about you from your social media provider. The profile information we receive may vary depending on the social media provider concerned, but will often include your name, email address, friends list, profile picture as well as other information you choose to make public on such social media platform. If you login using Facebook, we may also request access to other permissions related to your account, such as your friends, check-ins, and likes, and you may choose to grant or deny us access to each individual permission.</p>
                <p>We will use the information we receive only for the purposes that are described in this privacy notice or that are otherwise made clear to you on the relevant Services. Please note that we do not control, and are not responsible for, other uses of your personal information by your third-party social media provider. We recommend that you review their privacy notice to understand how they collect, use and share your personal information, and how you can set your privacy preferences on their sites and apps.</p>
            </div>
-           <div id="table-contents-7">
+           <div class="privacy-table-contents" id="table-contents-7">
                <h3>7. HOW LONG DO WE KEEP YOUR INFORMATION?</h3>
                <p><i><b>In Short:</b>  We keep your information for as long as necessary to fulfill the purposes outlined in this privacy notice unless otherwise required by law.</i></p>
                <p>We will only keep your personal information for as long as it is necessary for the purposes set out in this privacy notice, unless a longer retention period is required or permitted by law (such as tax, accounting or other legal requirements). No purpose in this notice will require us keeping your personal information for longer than the period of time in which users have an account with us.</p>
                <p>When we have no ongoing legitimate business need to process your personal information, we will either delete or anonymize such information, or, if this is not possible (for example, because your personal information has been stored in backup archives), then we will securely store your personal information and isolate it from any further processing until deletion is possible.</p>
            </div>
-           <div id="table-contents-8">
+           <div class="privacy-table-contents" id="table-contents-8">
                <h3>8. HOW DO WE KEEP YOUR INFORMATION SAFE?</h3>
                <p><i><b>In Short:</b>   We aim to protect your personal information through a system of organizational and technical security measures.</i></p>
                <p>We have implemented appropriate technical and organizational security measures designed to protect the security of any personal information we process. However, despite our safeguards and efforts to secure your information, no electronic transmission over the Internet or information storage technology can be guaranteed to be 100% secure, so we cannot promise or guarantee that hackers, cybercriminals, or other unauthorized third parties will not be able to defeat our security, and improperly collect, access, steal, or modify your information. Although we will do our best to protect your personal information, transmission of personal information to and from our Services is at your own risk. You should only access the Services within a secure environment.</p>
            </div>
-           <div id="table-contents-9">
+           <div class="privacy-table-contents" id="table-contents-9">
                <h3>9. DO WE COLLECT INFORMATION FROM MINORS?</h3>
                <p><i><b>In Short:</b>   We do not knowingly collect data from or market to children under 18 years of age.</i></p>
                <p>We do not knowingly solicit data from or market to children under 18 years of age. By using the Services, you represent that you are at least 18 or that you are the parent or guardian of such a minor and consent to such minor dependent’s use of the Services. If we learn that personal information from users less than 18 years of age has been collected, we will deactivate the account and take reasonable measures to promptly delete such data from our records. If you become aware of any data we may have collected from children under age 18, please contact us at <a href="mailto:support@wizrd.org">support@wizrd.org</a>.</p>
            </div>
-           <div id="table-contents-10">
+           <div class="privacy-table-contents" id="table-contents-10">
                <h3>10. WHAT ARE YOUR PRIVACY RIGHTS?</h3>
                <p><i><b>In Short:</b>  In Short:  In some regions, such as the European Economic Area, you have rights that allow you greater access to and control over your personal information. You may review, change, or terminate your account at any time.</i></p>
                 <p>In some regions (like the European Economic Area), you have certain rights under applicable data protection laws. These may include the right (i) to request access and obtain a copy of your personal information, (ii) to request rectification or erasure; (iii) to restrict the processing of your personal information; and (iv) if applicable, to data portability. In certain circumstances, you may also have the right to object to the processing of your personal information. To make such a request, please use the <a href="#table-contents-15">contact details</a> provided below. We will consider and act upon any request in accordance with applicable data protection laws.</p>
@@ -240,11 +240,11 @@ const PrivacyPolicyPage = (props) => {
                     <li>Access your account settings and update your preferences.</li>
                 </ul>
            </div>
-           <div id="table-contents-11">
+           <div class="privacy-table-contents" id="table-contents-11">
                <h3>11. CONTROLS FOR DO-NOT-TRACK FEATURES?</h3>
                <p>Most web browsers and some mobile operating systems and mobile applications include a Do-Not-Track ("DNT") feature or setting you can activate to signal your privacy preference not to have data about your online browsing activities monitored and collected. At this stage no uniform technology standard for recognizing and implementing DNT signals has been finalized. As such, we do not currently respond to DNT browser signals or any other mechanism that automatically communicates your choice not to be tracked online. If a standard for online tracking is adopted that we must follow in the future, we will inform you about that practice in a revised version of this privacy notice. </p>
            </div>
-           <div id="table-contents-12">
+           <div class="privacy-table-contents" id="table-contents-12">
                <h3>12. DO CALIFORNIA RESIDENTS HAVE SPECIFIC PRIVACY RIGHTS?</h3>
                <p><i><b>In Short:</b>  Yes, if you are a resident of California, you are granted specific rights regarding access to your personal information.</i></p>
                 <p>California Civil Code Section 1798.83, also known as the "Shine The Light" law, permits our users who are California residents to request and obtain from us, once a year and free of charge, information about categories of personal information (if any) we disclosed to third parties for direct marketing purposes and the names and addresses of all third parties with which we shared personal information in the immediately preceding calendar year. If you are a California resident and would like to make such a request, please submit your request in writing to us using the contact information provided below.</p>
@@ -393,12 +393,12 @@ const PrivacyPolicyPage = (props) => {
                 </ul>
                 <p>To exercise these rights, you can contact us Mailing to 16192 Coastal Highway, Lewes, Delaware 19958, or by referring to the contact details at the bottom of this document. If you have a complaint about how we handle your data, we would like to hear from you.</p>
            </div>
-           <div id="table-contents-13">
+           <div class="privacy-table-contents" id="table-contents-13">
                <h3>13. DO WE MAKE UPDATES TO THIS NOTICE?</h3>
                <p><i><b>In Short:</b>  Yes, we will update this notice as necessary to stay compliant with relevant laws.</i></p>
                <p>We may update this privacy notice from time to time. The updated version will be indicated by an updated "Revised" date and the updated version will be effective as soon as it is accessible. If we make material changes to this privacy notice, we may notify you either by prominently posting a notice of such changes or by directly sending you a notification. We encourage you to review this privacy notice frequently to be informed of how we are protecting your information.</p>
            </div>
-           <div id="table-contents-14">
+           <div class="privacy-table-contents" id="table-contents-14">
                <h3>14. HOW CAN YOU CONTACT US ABOUT THIS NOTICE?</h3>
                <p>If you have questions or comments about this notice, you may email us at support@wizrd.org or by post to:</p>
                <p>Wizrd Inc.<br/>
@@ -406,7 +406,7 @@ const PrivacyPolicyPage = (props) => {
                 Lewes, DE 19958<br/>
                 United States</p>
            </div>
-           <div id="table-contents-14">
+           <div class="privacy-table-contents" id="table-contents-14">
                <h3>15. HOW CAN YOU REVIEW, UPDATE OR DELETE THE DATA WE COLLECT FROM YOU?</h3>
                <p>Based on the applicable laws of your country, you may have the right to request access to the personal information we collect from you, change that information, or delete it in some circumstances. To request to review, update, or delete your personal information, please submit a request form by clicking here. We will respond to your request within 30 days.</p>
            </div>
