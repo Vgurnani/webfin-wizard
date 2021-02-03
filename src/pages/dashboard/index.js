0c82f2c836f187af01e5fb122f7de28e95df58b8
palak <pgupta@isystango.com>
2021-02-03 23:34:37 +0530

    Working on blog listing in dashboard.

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -19,7 +19,23 @@ import {
   SmallRadio,
   SmallRadioChecked,
   OpenArrow,
-} from '../../utils/svg'
+  DeleteIcon,
+  CloneIcon,
+} from '../../utils/svg';
+import
+  {
+    Container,
+    Row,
+    Col,
+    Form,
+    Button
+  }
+from 'react-bootstrap';
+import searchIcon from '../../images/search.png';
+import filterIcon from '../../images/filter.png';
+import EditIcon from '../../images/edit.png';
+
+
  const Dashboard =(props) => {
     const dispatch = useDispatch();
     const [status, setStatus ] = useState(false)
@@ -102,7 +118,7 @@ import {
         </aside>
         <main className="dashboard-data">
           <section className="dashboard-body">
-            <div className="dashboard-header">
+            {/* <div className="dashboard-header">
               <div className="dashboard-title">
                 <h1>Dashboard</h1>
                 <h5>
@@ -119,11 +135,249 @@ import {
                 <Link to={'/blog'} className='btn btn-primary'>Create Blog</Link>
               </div>
               
+            </div> */}
+            <div className="dashboard-header">
+              <div className="dashboard-title">
+                <h5>Site 1</h5>
+                <h1>My blog about food</h1>
+              </div>
+              <div className="dashboard-actions">
+              <Form className="search-form">
+                <Form.Group controlId="formBasicEmail">
+                    <Form.Control className="form-control" placeholder="Search" />
+                  </Form.Group>
+                  <Button className="btn-search" type="submit">
+                    <img src={searchIcon} />
+                  </Button>
+                </Form>
+                <a className="btn-filter">
+                  <img src={filterIcon} />
+                </a>
+              </div>
             </div>
-          
-            
-          
-            
+            <section className="dashboard-body">
+              <div className="dashboard-body-header">
+                <div className="dashboard-body-title">
+                  <h2>Posts</h2>
+                </div>
+                <div className="dashboard-body-actions">
+                  <a className="btn btn-primary">
+                  Add New+
+                  </a>
+                </div>
+              </div>
+              <div className="dashboard-table">
+                <div className="table-responsive">
+                  <table>
+                    <thead>
+                      <tr>
+                        <th>Title</th>
+                        <th>Views</th>
+                        <th>Comments</th>
+                        <th>Date Created</th>
+                        <th>Actions</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr>
+                        <td>
+                          <Form.Check 
+                            type="switch"
+                            id="custom-switch"
+                            label="The Joy of Cooking"
+                          /> 
+                        </td>
+                        <td>
+                        132 
+                        <span className="view-tags">★ Best Performance</span>
+                        </td>
+                        <td>
+                        32
+                        </td>
+                        <td>
+                        dd/mm/yyyy
+                        </td>
+                        <td>
+                          <a className="table-action-btns">
+                            <img src={EditIcon} />
+                            <span>Edit</span>
+                          </a>
+                          <a className="table-action-btns">
+                            <DeleteIcon />
+                            <span>Delete</span>
+                          </a>
+                          <a className="table-action-btns">
+                            <CloneIcon />
+                            <span>Clone</span>
+                          </a>
+                        </td>
+                      </tr>
+                      <tr>
+                        <td>
+                          <Form.Check 
+                            type="switch"
+                            id="custom-switch"
+                            label="The Joy of Cooking"
+                          /> 
+                        </td>
+                        <td>
+                        132 
+                        </td>
+                        <td>
+                        32
+                        </td>
+                        <td>
+                        dd/mm/yyyy
+                        </td>
+                        <td>
+                          <a className="table-action-btns">
+                            <img src={EditIcon} />
+                            <span>Edit</span>
+                          </a>
+                          <a className="table-action-btns">
+                            <DeleteIcon />
+                            <span>Delete</span>
+                          </a>
+                          <a className="table-action-btns">
+                            <CloneIcon />
+                            <span>Clone</span>
+                          </a>
+                        </td>
+                      </tr>
+                      <tr>
+                        <td>
+                          <Form.Check 
+                            type="switch"
+                            id="custom-switch"
+                            label="The Joy of Cooking"
+                          /> 
+                        </td>
+                        <td>
+                        132 
+                        </td>
+                        <td>
+                        32
+                        </td>
+                        <td>
+                        dd/mm/yyyy
+                        </td>
+                        <td>
+                          <a className="table-action-btns">
+                            <img src={EditIcon} />
+                            <span>Edit</span>
+                          </a>
+                          <a className="table-action-btns">
+                            <DeleteIcon />
+                            <span>Delete</span>
+                          </a>
+                          <a className="table-action-btns">
+                            <CloneIcon />
+                            <span>Clone</span>
+                          </a>
+                        </td>
+                      </tr>
+                      <tr>
+                        <td>
+                          <Form.Check 
+                            type="switch"
+                            id="custom-switch"
+                            label="The Joy of Cooking"
+                          /> 
+                        </td>
+                        <td>
+                        132 
+                        <span className="view-tags">★ Best Performance</span>
+                        </td>
+                        <td>
+                        32
+                        </td>
+                        <td>
+                        dd/mm/yyyy
+                        </td>
+                        <td>
+                          <a className="table-action-btns">
+                            <img src={EditIcon} />
+                            <span>Edit</span>
+                          </a>
+                          <a className="table-action-btns">
+                            <DeleteIcon />
+                            <span>Delete</span>
+                          </a>
+                          <a className="table-action-btns">
+                            <CloneIcon />
+                            <span>Clone</span>
+                          </a>
+                        </td>
+                      </tr>
+                      <tr>
+                        <td>
+                          <Form.Check 
+                            type="switch"
+                            id="custom-switch"
+                            label="The Joy of Cooking"
+                          /> 
+                        </td>
+                        <td>
+                        132 
+                        </td>
+                        <td>
+                        32
+                        </td>
+                        <td>
+                        dd/mm/yyyy
+                        </td>
+                        <td>
+                          <a className="table-action-btns">
+                            <img src={EditIcon} />
+                            <span>Edit</span>
+                          </a>
+                          <a className="table-action-btns">
+                            <DeleteIcon />
+                            <span>Delete</span>
+                          </a>
+                          <a className="table-action-btns">
+                            <CloneIcon />
+                            <span>Clone</span>
+                          </a>
+                        </td>
+                      </tr>
+                      <tr>
+                        <td>
+                          <Form.Check 
+                            type="switch"
+                            id="custom-switch"
+                            label="The Joy of Cooking"
+                          /> 
+                        </td>
+                        <td>
+                        132 
+                        </td>
+                        <td>
+                        32
+                        </td>
+                        <td>
+                        dd/mm/yyyy
+                        </td>
+                        <td>
+                          <a className="table-action-btns">
+                            <img src={EditIcon} />
+                            <span>Edit</span>
+                          </a>
+                          <a className="table-action-btns">
+                            <DeleteIcon />
+                            <span>Delete</span>
+                          </a>
+                          <a className="table-action-btns">
+                            <CloneIcon />
+                            <span>Clone</span>
+                          </a>
+                        </td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+            </section>
           </section>
         </main>
       </section>
