4a26b2a7cbc2b79a57194b27ace66feb0c61fbfe
palak <pgupta@isystango.com>
2021-02-24 22:03:06 +0530

    Worked on assesment buttons replacement.

--- src/pages/assessment/header.js
+++ src/pages/assessment/header.js
@@ -2,46 +2,53 @@ import React from 'react'
 import { Col, Row, Container, Button } from 'react-bootstrap'
 import webFinLogo from 'images/header/webFin-logo.svg';
 import { Link } from 'react-router-dom';
-import enterIcon from '../../public/images/enter-icon.png';
 import PropTypes from 'prop-types';
-
+import {
+    AssesmentNextButton,
+} from '../../utils/svg';
 const AssessmentHeader = (props) => {
     const { valid, prevPage , isFinalScreen, onSubmit } = props
     return (
-        <header style={ { height: '70pt' } } className={ 'main-header' }>
+        <header className={ 'main-header' }>
             <Container>
-                <Row className="header-top">
-                    <Col>
-                        { prevPage && <Button onClick={ prevPage } type="button" variant="secondary" >
-                            Back
-                        </Button> }
-                    </Col>
-                    <Col className="header-logo" style={ { textAlign: 'center' } }>
+                <Row className="header-top assesment-header">
+                    <Col className="header-logo">
                         <div className="navbar-brand">
                             <Link to={ '/' } className="navbar-item" title="Logo">
                                 <img src={ webFinLogo } alt="WebFin" />
                             </Link>
                         </div>
                     </Col>
-                    <Col>
-                        <div className="step-btn" style={ { float: 'right' } }>
-                            <span>
-                                { valid  ?
-                                    <Button type="button" onClick={ onSubmit }  variant="primary">
-                                        { isFinalScreen ? 'Finish!': 'Next' }
-                                    </Button>
-                                    :
-                                    <Button type="button" disabled={ true } variant="primary">
-                                        Next
-                                    </Button>}
-                            </span>
-                            <br/>
-                            <span className="enter-btn">
-                                <a>
-                                    Click next to proceed
-                                    <img src={ enterIcon } alt="Enter" />
-                                </a>
-                            </span>
+                    <Col className="assesment-left-btns">
+                        <div className="step-btns step-btns-left">
+                            <div className="step-btn">
+                                { prevPage && <Button onClick={ prevPage } type="button" variant="secondary" >
+                                    Back
+                                </Button> }
+                            </div>
+                        </div>
+                    </Col>
+
+                    <Col className="assesment-right-btns">
+                        <div className="step-btns step-btns-right">
+                            <div className="step-btn">
+                                <span>
+                                    { valid  ?
+                                        <Button type="button" onClick={ onSubmit }  variant="primary">
+                                            { isFinalScreen ? 'Finish!': 'Next' }
+                                        </Button>
+                                        :
+                                        <Button type="button" disabled={ true } variant="primary">
+                                            Next
+                                        </Button>}
+                                </span>
+                                <span className="enter-btn">
+                                    <a>
+                                        Click next to proceed
+                                        <AssesmentNextButton />
+                                    </a>
+                                </span>
+                            </div>
                         </div>
                     </Col>
                 </Row>
