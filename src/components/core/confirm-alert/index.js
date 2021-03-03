9587267e5abeb652bc9890834e5ec4c31f746fdd
palak <pgupta@isystango.com>
2021-03-03 18:41:48 +0530

    Designed confirm box.

--- src/components/core/confirm-alert/index.js
+++ src/components/core/confirm-alert/index.js
@@ -1,21 +1,36 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import closeIcon from '../../../../src/images/close.png'
 
 const ConfirmAlert = (props) => {
     const { onClose, handleAction } = props
     return (
-        <div className='custom-ui'>
-            <h3>Are you sure?</h3>
-            <p>You want to delete this?</p>
-            <button className="btn btn-secondry" onClick={ onClose }>No</button>
-            <button className="btn btn-secondry btn-danger"
-                onClick={ () => {
-                    handleAction()
-                    onClose();
-                } }
-            >
-                Yes, Delete it!
-            </button>
+        <div className='confirm-modal'>
+            <div className="confirm-modal-header">
+                <a className="confirm-modal-close"  onClick={ onClose }>
+                    <img src={ closeIcon } />
+                </a>
+            </div>
+            <div className="confirm-modal-body">
+                <h2>Are you sure?</h2>
+                <p>By confirming this blog & information will be deleted forever.</p>
+                {/* <p>You want to delete this?</p> */}
+                <div className="confirm-modal-btns">
+                    <div className="confirm-btn">
+                        <button className="btn btn-primary"
+                            onClick={ () => {
+                                handleAction()
+                                onClose();
+                            } }
+                        >
+                            Confirm
+                        </button>
+                    </div>
+                    <div className="cancel-btn">
+                        <button className="btn" onClick={ onClose }>Cancel</button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
