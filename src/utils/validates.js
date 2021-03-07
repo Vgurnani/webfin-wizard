dc37d08a2d4022cab8c1ea633b0aaa705b1f17f7
Vaibhav <vaibhavgstpl@gmail.com>
2021-03-07 16:37:18 +0530

    Work on 2828 1> Removed confirm password field. 2> Fixed lint issues.

--- src/utils/validates.js
+++ src/utils/validates.js
@@ -13,42 +13,20 @@ export const loginValidate = values => {
 
 export const registerValidate = values => {
     const errors = {};
-    const letter = /^[a-zA-Z][a-zA-Z\s]*$/;
-    if(!values.firstName){
-        errors.firstName = MESSAGE.VALID_ENTER('first name');
-    }
-    if(!values.lastName){
-        errors.lastName = MESSAGE.VALID_ENTER('last name');
-    }
-    if(values.firstName && !values.firstName.match(letter)){
-        errors.firstName = MESSAGE.SHOULD_CHAR;
-    }
-    if(values.lastName && !values.lastName.match(letter)){
-        errors.lastName = MESSAGE.SHOULD_CHAR;
-    }
+
     if (!values.email) {
         errors.email = MESSAGE.VALID_ENTER('email');
     }
     if(values.email && (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email))){
         errors.email = MESSAGE.INVALID_EMAIL;
     }
-
     if (!values.password) {
         errors.password = MESSAGE.VALID_ENTER('password');
     }
     if (values.password && values.password.length < 8 ) {
         errors.password = MESSAGE.SHOULD_BE_LENGTH('password',8);
     }
-    if (!values.confirmPassword) {
-        errors.confirmPassword = MESSAGE.VALID_ENTER('confirm password');
-    }
-    if (values.confirmPassword && values.confirmPassword.length < 8 ) {
-        errors.confirmPassword = MESSAGE.SHOULD_BE_LENGTH('confirm password',8);
-    }
 
-    if( values.password !== values.confirmPassword){
-        errors.confirmPassword = MESSAGE.PASSWORD_MATCH;
-    }
     return errors;
 }
 
