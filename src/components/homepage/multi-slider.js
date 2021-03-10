429b579a2785b39ca76ffbe89a96045e1aaa241a
palak <pgupta@isystango.com>
2021-03-11 00:45:42 +0530

    Worked on home page UI.

--- src/components/homepage/multi-slider.js
+++ src/components/homepage/multi-slider.js
@@ -6,20 +6,20 @@ import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
 const responsive = {
     superLargeDesktop: {
-        // the naming can be any, depends on you.
         breakpoint: { max: 4000, min: 3000 },
         items: 5
     },
     desktop: {
-        breakpoint: { max: 3000, min: 1024 },
-        items: 3
+        breakpoint: { max: 3000, min: 1279 },
+        items: 3,
+        partialVisibilityGutter: 40
     },
     tablet: {
-        breakpoint: { max: 1024, min: 464 },
+        breakpoint: { max: 1279, min: 767 },
         items: 2
     },
     mobile: {
-        breakpoint: { max: 464, min: 0 },
+        breakpoint: { max: 767, min: 0 },
         items: 1
     }
 };
@@ -55,7 +55,37 @@ const MultipleSlider = () =>  (
                     <div className="plan-img">
                         <img src={ basicPlan } alt="Basic Plan" />
                     </div>
-                    <h4 className="plan-name">Wizrd</h4>
+                    <h4 className="plan-name">
+                        <span>Wizrd</span>
+                        <span className="plan_tag">Plus</span>
+                    </h4>
+                    <ul className="plan-feature">
+                        <li>Get a beautiful blog in seconds!</li>
+                        <li>1 Website + 1 Custom Sub-Domain</li>
+                        <li>1,000 Views / Month</li>
+                        <li>1 user</li>
+                        <li>100 MB of space </li>
+                        <li>2 Blog posts / Month </li>
+                        <li>No credit card required </li>
+                    </ul>
+                    <div className="plan-price">Free</div>
+                    <div className="plan-action">
+                        <button className="btn btn-secondary">Try Now</button>
+                        <span className={ 'getText mt-3' }>Get Yours In Minutes</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div>
+            <div className="plan-detail">
+                <div className="plan-detail-inner">
+                    <div className="plan-img">
+                        <img src={ basicPlan } alt="Basic Plan" />
+                    </div>
+                    <h4 className="plan-name">
+                        <span>Wizrd</span>
+                        <span className="plan_tag primary-color">Pro</span>
+                    </h4>
                     <ul className="plan-feature">
                         <li>Get a beautiful blog in seconds!</li>
                         <li>1 Website + 1 Custom Sub-Domain</li>
@@ -103,31 +133,10 @@ const MultipleSlider = () =>  (
                     <div className="plan-img">
                         <img src={ basicPlan } alt="Basic Plan" />
                     </div>
-                    <h4 className="plan-name">Wizrd</h4>
-                    <ul className="plan-feature">
-                        <li>Get a beautiful blog in seconds!</li>
-                        <li>1 Website + 1 Custom Sub-Domain</li>
-                        <li>1,000 Views / Month</li>
-                        <li>1 user</li>
-                        <li>100 MB of space </li>
-                        <li>2 Blog posts / Month </li>
-                        <li>No credit card required </li>
-                    </ul>
-                    <div className="plan-price">Free</div>
-                    <div className="plan-action">
-                        <button className="btn btn-secondary">Try Now</button>
-                        <span className={ 'getText mt-3' }>Get Yours In Minutes</span>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div>
-            <div className="plan-detail">
-                <div className="plan-detail-inner">
-                    <div className="plan-img">
-                        <img src={ basicPlan } alt="Basic Plan" />
-                    </div>
-                    <h4 className="plan-name">Wizrd</h4>
+                    <h4 className="plan-name">
+                        <span>Wizrd</span>
+                        <span className="plan_tag">Pro</span>
+                    </h4>
                     <ul className="plan-feature">
                         <li>Get a beautiful blog in seconds!</li>
                         <li>1 Website + 1 Custom Sub-Domain</li>
